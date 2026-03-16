458deda8a31f53ec71fff2aa5adb98647dd95a4a
Tirth Patel <111308497+TirthWillLearn@users.noreply.github.com>
2026-03-16 22:39:23 +0530

    refactor: improve Express server setup with logging, rate limiting, and proper middleware order

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+
 import { pool } from "./config/db";
 import authRoutes from "./routes/authRoutes";
 import productRoutes from "./routes/product.routes";
 import orderRoutes from "./routes/order.routes";
 import { authenticate } from "./middlewares/authMiddleware";
 import { errorHandler } from "./middlewares/error.middleware";
-import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -16,29 +17,35 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
-
-app.use("/auth", authRoutes);
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
-
-app.get("/protected", authenticate, (req, res) => {
-  res.json({ message: "You accessed a protected route" });
-});
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 
 app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/protected", authenticate, (req, res) => {
+  res.json({ message: "You accessed a protected route" });
+});
+
+app.use("/auth", authRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+/* 404 handler */
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+/* global error handler */
+app.use(errorHandler);
 
 pool
   .connect()
@@ -48,5 +55,5 @@ pool
 const PORT = process.env.PORT || 4243;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
